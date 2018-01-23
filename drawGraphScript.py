45acb5df0acbcfc8ae8543962eb252764398f4e2
Lucas-Rapahel Müller <gitdev@LRM.PHOTO>
2018-01-23 21:52:44 +0100

    Finish plotting

--- drawGraphScript.py
+++ drawGraphScript.py
@@ -2,14 +2,11 @@ import networkx as nx
 from nxpd import draw
 import matplotlib.pyplot as plt
 
-from graphManipulation import read_graph_from_file, draw_graph, add_visited_attribute_to_edge
+from graphManipulation import read_graph_from_file, draw_graph
 
 
 if __name__ == '__main__':
-    G = read_graph_from_file(path='data/att48_d.txt', delimiter=' ', path_xy='data/att48_xy.txt')
-    # position is stored as node attribute data for random_geometric_graph
+    G = read_graph_from_file('data/att48_d.txt', ' ')#, 'data/att48_xy.txt')
 
-    G = add_visited_attribute_to_edge(G, [0, 1, 2])
-
-    draw_graph(G)
+    draw_graph(G, [0, 1, 2])
     plt.show()
--- graphManipulation.py
+++ graphManipulation.py
@@ -1,54 +1,52 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
 
 
-def read_graph_from_file(path, delimiter, path_xy=None):
+
+def read_graph_from_file(file_dist_mat, delimiter, file_xy_mat=None):
     """ Reads graph from file. Unrecognised lines should be commented by a #."""
     G = nx.Graph()
-    dist_matrix = np.loadtxt(path, dtype='f', delimiter=delimiter)
+    dist_matrix = np.loadtxt(file_dist_mat, dtype='f', delimiter=delimiter)
 
     n = dist_matrix.shape[0]
     for i in range(n):
         for j in range(n)[i:]:
             G.add_edge(i, j, weight=dist_matrix[i,j])
-                
-    if path_xy is not None:
-        xy_mat = np.loadtxt(path_xy, dtype='f', delimiter=delimiter)
+
+    if file_xy_mat is not None:
+        xy_mat = np.loadtxt(file_xy_mat, dtype='f', delimiter=delimiter)
         nx.set_node_attributes(G, (None, None), 'pos')
-        for i in range(n):
-            G.node[i]['pos'] = (xy_mat[i,0], xy_mat[i,1])
+    else:
+        """If xy matrix is not provided, calculate coordinates from principal component analysis"""
+        # xy_pca = PCA(n_components=2)
+        # xy_mat = xy_pca.fit_transform(dist_matrix)
+        xy_mds = MDS(n_components=2)
+        xy_mat = xy_mds.fit_transform(dist_matrix)
+        nx.set_node_attributes(G, (None, None), 'pos')
+    for i in range(n):
+        G.node[i]['pos'] = (xy_mat[i,0], xy_mat[i,1])
             
     return G
 
 
-def add_visited_attribute_to_edge(G, path):
-    nx.set_edge_attributes(G, False, 'visited')
-    for i in range(path.__len__() - 1):
-        G[path[i]][path[i + 1]]['visited'] = True
-
-    return G
-
-
-def draw_graph(G, file_name='graph.pdf'):
+def draw_graph(G, path, file_name='graph.pdf'):
     """Simple representation of the graph featuring labels of a given attribute."""
     # position is stored as node attribute data for random_geometric_graph
     pos = nx.get_node_attributes(G,'pos')
-    visited = nx.get_edge_attributes(G, 'visited')
 
-    for key, value in visited.items():
-        if value:
-            visited[key] = 1
-        else:
-            visited[key] = 0
+    edges_on_path = [None] * (path.__len__() - 1)
+    for i in range(edges_on_path.__len__()):
+        edges_on_path[i] = (path[i], path[i + 1])
+    edges_on_path = tuple(edges_on_path)
 
-    plt.figure(figsize=(8, 8))
-    nx.draw_networkx_edges(G, pos, alpha=0.4,
-                           edge_color=list(visited.values()),
-                           edge_vmin=0,
-                           edge_vmax=1)
 
-    nx.draw_networkx_nodes(G, pos, node_size=50)
+    f = plt.figure(figsize=(10, 10))
+    nx.draw_networkx_edges(G, pos, alpha=0.1)
+    nx.draw_networkx_edges(G, pos, alpha=0.8, edgelist=edges_on_path, edge_color='r')
+    nx.draw_networkx_nodes(G, pos, node_size=80, node_color='k')
 
     plt.axis('off')
     plt.savefig(file_name)
