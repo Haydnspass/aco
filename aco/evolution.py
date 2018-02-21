c0545b5f2426bd9d917e728b76e1ca9fa9041aaf
Lucas-Rapahel Müller <gitdev@LRM.photo>
2018-02-21 20:28:34 +0100

    Compliance to PEP standard

--- aco/evolution.py
+++ aco/evolution.py
@@ -6,6 +6,7 @@ from heapq import *
 import evaluation
 import os
 
+
 class Evolution():
     ''' class to let several ant colonies compete agains each other to determine good parameters for the evaluated
     algorithm
@@ -28,31 +29,31 @@ class Evolution():
             count (int): running count acting as a colony id
     '''
 
-    def __init__(self, colonies, ants, algo, iter, init_params, graph, unique_visit, goal, start_node, end_node, \
-                 tries, epochs, variation, drop_out):
+    def __init__(self, colonies, ants, algo, iter, init_params, graph, unique_visit, goal,
+                 start_node, end_node, tries, epochs, variation, drop_out):
 
-        #ants
+        # ants
         self.num_colonies = colonies
         self.num_ants = ants
         self.algo = algo
         self.iter = iter
         self.init_genes = init_params
 
-        #world
+        # world
         self.graph = graph
         self.unique_visit = unique_visit
         self.goal = goal
         self.start = start_node
         self.end = end_node
 
-        #evolution
+        # evolution
         self.tries = tries
         self.epochs = epochs
         self.variation = variation
         self.drop_out = drop_out
         self.count = 0
 
-        #the 6th day
+        # the 6th day
         print('genesis')
         self.population = []
         for _ in range(self.num_colonies):
@@ -70,22 +71,22 @@ class Evolution():
                 genes (dict)
                 id (current count)
         '''
-
-        #variation
+        # variation
         genes = deepcopy(parent_genes)
         for key in parent_genes.keys():
-            if parent_genes[key] != None:
+            if parent_genes[key] is not None:
                 factor = np.random.uniform(1 - self.variation, 1 + self.variation)
                 genes[key] *= factor
 
-        #initialization
-        child = AntColony(graph=self.graph, ants_total=self.num_ants, iter=self.iter, \
-                          alpha=genes['alpha'], beta=genes['beta'], rho=genes['rho'], unique_visit=self.unique_visit,\
-                          goal=self.goal, start_node=self.start, end_node=self.end, init_pher=genes['init_pher'], \
-                          min_pher=genes['min_pher'], max_pher=genes['max_pher'], q0=genes['q0'], tau=genes['tau'], \
-                          algo=self.algo)
+        # initialization
+        child = AntColony(graph=self.graph, ants_total=self.num_ants, iter=self.iter,
+                          alpha=genes['alpha'], beta=genes['beta'], rho=genes['rho'],
+                          unique_visit=self.unique_visit, goal=self.goal, start_node=self.start,
+                          end_node=self.end, init_pher=genes['init_pher'],
+                          min_pher=genes['min_pher'], max_pher=genes['max_pher'], q0=genes['q0'],
+                          tau=genes['tau'], algo=self.algo)
         count = self.count
-        #print('colony', self.count, 'is born')
+        # print('colony', self.count, 'is born')
         self.count += 1
 
         return child, genes, count
@@ -127,26 +128,26 @@ class Evolution():
 
             print('\nEpoch:', i)
 
-            #selection
+            # selection
             num_survivors = int(self.num_colonies * (1 - self.drop_out))
             parents = nsmallest(num_survivors, self.population)
 
-            #reproduction and evaluation
+            # reproduction and evaluation
             children = []
 
-            #make sure enough offspring is produced in case drop_out > 0.5
+            # make sure enough offspring is produced in case drop_out > 0.5
             while len(children) < self.num_colonies - len(parents):
 
-                #most successful colony reproduces most often
+                # most successful colony reproduces most often
                 for parent in parents:
 
-                    #keep total population
+                    # keep total population
                     if len(children) == self.num_colonies - len(parents):
                         break
 
                     print('colony', parent[1], 'is reproducing')
 
-                    #birth
+                    # birth
                     parent_genes = parent[3]
                     child, child_genes, child_count = self.make_colony(parent_genes)
                     fitness = self.eval_fitness(child)
@@ -155,11 +156,11 @@ class Evolution():
             self.population = parents + children
             heapify(self.population)
 
-            #epochs shortest distance
+            # epochs shortest distance
             epoch_winner = self.population[0]
             shortest_distances.append(epoch_winner[0])
 
-            #epochs mean distance and std
+            # epochs mean distance and std
             all_distances = np.array([self.population[i][0] for i in range(self.num_colonies)])
             mean = np.mean(all_distances)
             std = np.std(all_distances)
@@ -167,7 +168,7 @@ class Evolution():
 
             print([(self.population[i][0], self.population[i][1]) for i in range(len(self.population))])
 
-            #saving history
+            # saving history
             memory[i, 0] = mean
             memory[i, 1] = std
             memory[i, 2] = epoch_winner[0]
@@ -175,7 +176,7 @@ class Evolution():
             if path:
                 np.save(path, memory)
 
-        #leaves the heap invariant
+        # leaves the heap invariant
         alpha_distance, alpha_id, alpha_colony, alpha_genes = heappop(self.population)
 
         return alpha_colony, alpha_genes, memory
@@ -183,17 +184,20 @@ class Evolution():
 
 if __name__ == '__main__':
 
-    gene_root = {'alpha': 1, 'beta': 1, 'rho': 0.1, 'init_pher': None, 'min_pher': None, 'max_pher': None, 'q0': None, 'tau': None}
+    gene_root = {'alpha': 1, 'beta': 1, 'rho': 0.1, 'init_pher': None, 'min_pher': None,
+                 'max_pher': None, 'q0': None, 'tau': None}
 
-    #G = simple_cube()
+    # G = simple_cube()
     G = read_graph_from_file(path='data/oliver30.txt', delimiter=' ')
 
-    evolution = Evolution(colonies=2, ants=10, algo='ant_system', iter=10, init_params=gene_root, graph=G, unique_visit=True, \
-                    goal='TSP', start_node=None, end_node=None, tries=1, epochs=10, variation=0.5, drop_out=0.5)
+    evolution = Evolution(colonies=2, ants=10, algo='ant_system', iter=10, init_params=gene_root,
+                          graph=G, unique_visit=True, goal='TSP', start_node=None, end_node=None,
+                          tries=1, epochs=10, variation=0.5, drop_out=0.5)
 
     alpha_colony, alpha_genes, memory = evolution.begin(path='data/evo_hist.npy')
-    #evaluation.plot_evolution_hist(shortest_distances, mean_distances, path='plots/evo_test.pdf', title='Evo Test')
+    # evaluation.plot_evolution_hist(shortest_distances, mean_distances, path='plots/evo_test.pdf',
+    #                                title='Evo Test')
 
-    #print('\nwinner:')
-    #print(alpha_colony.shortest_dist)
-    #print(alpha_genes)
+    # print('\nwinner:')
+    # print(alpha_colony.shortest_dist)
+    # print(alpha_genes)
